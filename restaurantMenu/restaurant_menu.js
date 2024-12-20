2fcf5d528c5300301c8e4d9ce949be7a65a42f4b
ic3000 <cickovskij@gmail.com>
2024-12-20 17:42:52 +0200

    commit for employee details

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,4 +1,4 @@
-const breakfastMenu = ['Pancakes - $5', 'Eggs Benedict - $4', 'Oatmeal - $4', 'Frittata - $3'];
+const breakfastMenu = ['Pancakes - $15', 'Eggs Benedict - $4', 'Oatmeal - $4', 'Frittata - $3'];
 const mainCourseMenu = ['Steak - $5', 'Pasta - $6', 'Burger - $4', 'Salmon - $7'];
 const dessertMenu = ['Cake - $5', 'Ice Cream - $2', 'Pudding - $2', 'Fruit Salad - $4'];
 
