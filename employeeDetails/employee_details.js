74548f8687e3d77615e36ac81e160a54225e3e66
ic3000 <cickovskij@gmail.com>
2024-12-20 18:49:55 +0200

    commit for exersize update

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,14 +1,14 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-    { id: 4, name: 'Igor Cickovskij', age: 50, department: 'IT', salary: 160000 },
-    { id: 5, name: 'Eva Brown', age: 18, department: 'HR', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'JavaScript' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Python' },
+    { id: 4, name: 'Igor Cickovskij', age: 50, department: 'IT', salary: 160000, specialization: 'Rust' },
+    { id: 5, name: 'Eva Brown', age: 18, department: 'HR', salary: 60000, specialization: 'Rust' },
     //... More employee records can be added here
   ];
  // Function to display all employees
  function displayEmployees() {
- const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}; name: ${employee.name}, age: ${employee.age}, department - ${employee.department}, salary - $${employee.salary}</p>`).join('');
+ const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}; name: ${employee.name}, age: ${employee.age}, department - ${employee.department}, salary - $${employee.salary}, specialization - ${employee.specialization}</p>`).join('');
  document.getElementById('employeesDetails').innerHTML = totalEmployees;
  }
 function calculateTotalSalaries() {
@@ -28,4 +28,9 @@ function findEmployeeById(employeeId) {
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
+}
+function findEmployeeBySpec() {
+  const hrEmployees = employees.filter(employee => employee.specialization === 'Rust');
+     const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
+     document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
 }
